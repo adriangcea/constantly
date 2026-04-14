b1bf65041172496821c01f7fff4186ba61c4852f
Adrian <adriangcea@gmail.com>
2026-04-15 01:04:36 +0200

    Implementacion de Flask, conexion con la base de datos y creacion de endpoint post y get de usuarios

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from db.conexion import get_connection
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "API de hábitos funcionando 🚀"
-
-@app.route("/usuarios")
+@app.route("/usuarios", methods=["GET"])
 def usuarios():
-    return jsonify([
-        {"id": 1, "nombre": "Admin"},
-        {"id": 2, "nombre": "Usuario1"}
-    ])
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM Usuario")
+    result = cursor.fetchall()
+
+    conn.close()
+    return jsonify(result)
+
+
+@app.route("/usuarios", methods=["POST"])
+def crear_usuario():
+    data = request.json
+
+    nombre = data["nombre"]
+    email = data["email"]
+    contraseña = data["contraseña"]
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO Usuario (nombre, email, contraseña, fecha_registro, rol, estado)
+    VALUES (%s, %s, %s, NOW(), 'user', 'activo')
+    """
+
+    cursor.execute(query, (nombre, email, contraseña))
+    conn.commit()
+
+    conn.close()
+
+    return jsonify({"mensaje": "Usuario creado correctamente"})
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
