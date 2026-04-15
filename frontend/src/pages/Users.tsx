ca749aa243eba243ffc3a4254f69cda3787eb57c
Adrian <adriangcea@gmail.com>
2026-04-16 00:13:29 +0200

    Añadimos funcionalidad para crear usuarios y verlos en lista

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -1,16 +1,82 @@
 import { useEffect, useState } from "react";
-import { getUsuarios } from "../services/api";
+import { getUsuarios, createUsuario } from "../services/api";
 
 const Users = () => {
   const [usuarios, setUsuarios] = useState<any[]>([]);
 
+  // formulario
+  const [form, setForm] = useState({
+    nombre: "",
+    email: "",
+    contraseña: "",
+  });
+
+  // cargar usuarios
   useEffect(() => {
-    getUsuarios().then(data => setUsuarios(data));
+    loadUsers();
   }, []);
 
+  const loadUsers = () => {
+    getUsuarios().then((data) => setUsuarios(data));
+  };
+
+  // actualizar inputs
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //enviar formulario (POST)
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await createUsuario(form);
+
+    setForm({
+      nombre: "",
+      email: "",
+      contraseña: "",
+    });
+
+    loadUsers(); // refrescar lista
+  };
+
   return (
-    <div>
-      <h2>Lista de usuarios</h2>
+    <div style={{ padding: "20px" }}>
+      <h2>Usuarios</h2>
+
+      {/*FORMULARIO CREAR USUARIO*/}
+      <form onSubmit={handleSubmit}>
+        <input
+          name="nombre"
+          placeholder="Nombre"
+          value={form.nombre}
+          onChange={handleChange}
+        />
+
+        <input
+          name="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={handleChange}
+        />
+
+        <input
+          name="contraseña"
+          type="password"
+          placeholder="Contraseña"
+          value={form.contraseña}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Crear usuario</button>
+      </form>
+
+      <hr />
+
+      {/*LISTA DE USUARIOS*/}
       <ul>
         {usuarios.map((u) => (
           <li key={u.id_usuario}>
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,3 +4,19 @@ export const getUsuarios = async () => {
   const response = await fetch(`${API_URL}/usuarios`);
   return response.json();
 };
+
+export const createUsuario = async (usuario: {
+  nombre: string;
+  email: string;
+  contraseña: string;
+}) => {
+  const res = await fetch(`${API_URL}/usuarios`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(usuario),
+  });
+
+  return res.json();
+};
