46d1b47804c20f7ead1c4c089b9a92458f0c177e
Adrian <adriangcea@gmail.com>
2026-04-28 01:00:21 +0200

    actualización de app.py

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,20 @@
 from flask import Flask 
 from flask_cors import CORS
-from routes.usuarios import usuarios_bp
- 
+from flask_jwt_extended import JWTManager
+from routes.users import users_bp
+from routes.auth import auth_bp
+from routes.habits import habits_bp 
+
 app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "super-secret-key"
+jwt = JWTManager(app) 
 
 CORS(app)
 
-app.register_blueprint(usuarios_bp)
+app.register_blueprint(users_bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(habits_bp)   
 
 if __name__ == "__main__":
     app.run(debug=True)
