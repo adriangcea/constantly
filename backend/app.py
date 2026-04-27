f93894cf4d6a36feecf4ce95178144b0ab88ee1b
Adrian <adriangcea@gmail.com>
2026-04-28 01:44:15 +0200

    implementamos en app.py las funcionalidades de progress.py

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_jwt_extended import JWTManager
 from routes.users import users_bp
 from routes.auth import auth_bp
 from routes.habits import habits_bp 
+from routes.progress import progress_bp
 
 app = Flask(__name__)
 
@@ -14,7 +15,8 @@ CORS(app)
 
 app.register_blueprint(users_bp)
 app.register_blueprint(auth_bp)
-app.register_blueprint(habits_bp)   
+app.register_blueprint(habits_bp)  
+app.register_blueprint(progress_bp) 
 
 if __name__ == "__main__":
     app.run(debug=True)
