c80b8350536ab51f711242063896bf88be956ece
Adrian <adriangcea@gmail.com>
2026-04-28 00:52:50 +0200

    Creamos habits.py y creamos el endpoint post para la creación de usuarios

---
+++ backend/routes/habits.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from db.connection import get_connection
+
+habits_bp = Blueprint("habits", __name__)
+
+@habits_bp.route("/habits", methods=["POST"])
+@jwt_required()
+def create_habit():
+    user_id = get_jwt_identity()
+    data = request.get_json()
+
+    # Validación
+    if not data or not all(k in data for k in ("name", "frequency")):
+        return jsonify({"mensaje": "Datos incompletos"}), 400
+
+    name = data["name"]
+    description = data.get("description", "")
+    frequency = data["frequency"]
+
+    # Validar ENUM
+    if frequency not in ["diaria", "semanal", "mensual"]:
+        return jsonify({"mensaje": "Frecuencia inválida"}), 400
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+        INSERT INTO Habito (id_usuario, nombre, descripcion, frecuencia)
+        VALUES (%s, %s, %s, %s)
+    """
+
+    try:
+        cursor.execute(query, (user_id, name, description, frequency))
+        conn.commit()
+    except Exception as e:
+        return jsonify({"mensaje": "Error al crear el hábito"}), 500
+
+    cursor.close()
+    conn.close()
+
+    return jsonify({"mensaje": "Hábito creado exitosamente"}), 201
